239d1bf0764c5b79f29d52c61be89de17f9922b1
Gabe Cook <7717888+gabe565@users.noreply.github.com>
2025-11-30 01:26:33 +0000

    pixlet: update 0.49.2 bottle.

--- Formula/pixlet.rb
+++ Formula/pixlet.rb
@@ -8,8 +8,8 @@ class Pixlet < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/tronbyt/tronbyt"
-    sha256 cellar: :any,                 arm64_tahoe:  "99b7963fcbd6aa1f612ce480bae0ad8952736dc030a88f295e23e4f4e9792427"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "f960a05b766b2773e140ab8773ad295d14bd308a8369fef1d803ea6f9ffcdc0a"
+    sha256 cellar: :any,                 arm64_tahoe:  "1dbae1aac7cf0bcb2580980a4697baaab0dbad571994915516103e43d017a6ff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b846e7f593509c59146c1ec80a8414a3f72ce4b911fa48a20dfaf527365b24df"
   end
 
   depends_on "go" => :build
